f4e0513c59da67a40eee726601454a13e78fbf76
Kevyn Rocha <kevyn_oliver@hotmail.com>
2022-03-22 23:14:34 -0400

    fix ci

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,9 +6,19 @@ for dir in "${dirs[@]}"
 do
     cd $dir
     flutter pub get
-    flutter analyze
+    OUTPUT="$(flutter analyze)"
+    echo "$OUTPUT"
+    echo
+    if grep -q "error •" <<< "$OUTPUT"; then
+        echo "flutter analyze found errors"
+        exit 1
+    else
+        echo "flutter analyze didn't find any errors"
+        exit 0
+    fi
+    # flutter analyze
     # flutter format --dry-run --set-exit-if-changed .
     # flutter test --coverage
-    flutter test --machine >> test-results.json
+    # flutter test --machine >> test-results.json
     cd ../../
 done
