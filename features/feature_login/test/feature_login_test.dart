c5ef308e750e0898f4002dfd95f01b5e7ef30c4e
Kevyn Rocha <kevyn_oliver@hotmail.com>
2022-03-23 00:39:00 -0400

    fix ci

--- features/feature_login/test/feature_login_test.dart
+++ features/feature_login/test/feature_login_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('FeatureLogin', () {
     test('should return 1 too', () {
-      expect(1, 0);
+      expect(1, 1);
     });
   });
 }
